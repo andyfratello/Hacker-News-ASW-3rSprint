67bd53553a55b339b1841bd77df071561fb2860c
david <david.peba123@gmail.com>
2022-12-22 20:58:24 +0100

    show url valid

--- src/views/Submit.vue
+++ src/views/Submit.vue
@@ -3,8 +3,9 @@
     <h2>Submit</h2>
     <input type="text" placeholder="title" name="title" v-model="posts.title" required> <br> <br>
     <input type="text" placeholder="url" name="url" v-model="posts.url"> <br> <br>
+    <div v-if="isValid" class="url-valid"> URL invalid </div>
     <textarea placeholder="text" name="text" v-model="posts.text" required></textarea> <br> <br>
-    <button type="submit" v-on:click= "postData" class="button-submit">Submit</button>
+    <button type="submit" v-on:click="postData" class="button-submit">Submit</button>
   </div>
 </template>
 
@@ -13,6 +14,7 @@ import axios from 'axios'
 import {globalStore} from '../model/sesion.js'
 
 const BASE_URL = 'https://mysite-mnjc.onrender.com/'
+const regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
 
 export default {
   name: 'Submit',
@@ -45,6 +47,14 @@ export default {
           console.log(err)
         })
     }
+  },
+  computed: {
+    isValid () {
+      console.log('entro')
+      console.log('Es: ' + regex.test(this.posts.url))
+      if (this.posts.url === '') return false
+      return !regex.test(this.posts.url)
+    }
   }
 }
 </script>
@@ -52,5 +62,12 @@ export default {
 <style>
 .button-submit {
   min-height: 36px;
+}
+.url-valid{
+  font-size: 15px;
+  padding-left: 0.5em;
+  padding-bottom: 0.5em;
+  margin-top: -0.7em;
+  color: red;
 }
 </style>
